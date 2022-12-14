b878ff0efe19ea8a40df3e796a3d12f8083f0e30
Slinick <119591833+Slinick@users.noreply.github.com>
2022-12-14 18:45:48 +0100

    change

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -5,6 +6,20 @@ public class Main {
         Cats mycat = new Cats("Tom", true, 0);
         mycat.say("miiiiiiiiiiiiiiy");
 
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("введите имя кота");
+        String name = scan.nextLine();
+
+        System.out.println("это хороший кот?");
+        Boolean q = scan.nextBoolean();
+
+        System.out.println("возраст кота?");
+        Integer ag = scan.nextInt();
+
+        Cats mycat2 = new Cats(name, q, ag);
+
         System.out.println(mycat);
+        System.out.println(mycat2);
     }
 }
