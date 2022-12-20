1988a2dc9dafa415a759a57da1562f2f19e58a43
Slinick <119591833+Slinick@users.noreply.github.com>
2022-12-20 08:20:08 +0100

    778

--- src/Practice/ParseInt2.java
+++ src/Practice/ParseInt2.java
@@ -7,7 +7,7 @@ class ParseInt2 {
         String str, str2 ="";
         Integer i, result;
         Boolean flag;
-        //только вчера встретил этот do - while надо попрактиковать его
+        //do - while
         do {
             flag = false;
             Scanner scanner = new Scanner(System.in);
@@ -20,7 +20,7 @@ class ParseInt2 {
                         str2.equals("3")|str2.equals("4")|str2.equals("5")|
                         str2.equals("6")|str2.equals("7")|str2.equals("8")|
                         str2.equals("9")){
-                }else flag = true;
+                }else flag = true; break;
             }
         }while(flag);
         System.out.println(Integer.parseInt(str)*2);
