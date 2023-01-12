3ebd79e4d63e82120add65e108c9ce0520eba116
Slinick <119591833+Slinick@users.noreply.github.com>
2023-01-12 11:27:04 +0100

    780

--- src/Practice/TaskFromBook1/MorskojBoj/MorskojBoj_version1_01.java
+++ src/Practice/TaskFromBook1/MorskojBoj/MorskojBoj_version1_01.java
@@ -33,22 +33,43 @@ class MorskojBoj_version1_01 {
                 System.out.println();
             }
 // поле готово начинаем битву
-    int[][] myarray = array;
-    Scanner scaner2 = new Scanner(System.in);
-    System.out.println("Делайте выстрел. На поле есть 3Х корабль. (например 00)");
-    String x1 = scaner2.nextLine();
-    int x2 = Integer.parseInt(x1.charAt(0) + "");
-    int y2 = Integer.parseInt(x1.charAt(1) + "");
-    System.out.println(x2 + "   " + y2);
-    myarray2[x2][y2] = "*";
-    //проверяем ход
-        if (myarray[x2][y2] == 1) {
-            count ++;
-            myarray2[x2][y2] = "X";
-            if (count == 3) System.out.println("ПОБЕДА!!!");
-            else System.out.println("Корабль подбит!!! ");
+            int[][] myarray = array;
+            Scanner scaner2 = new Scanner(System.in);
+            System.out.println("Делайте выстрел. На поле есть 3Х корабль. (например 00)");
+            String x1 = scaner2.nextLine();
+            int x2 = Integer.parseInt(x1.charAt(0) + "");
+            int y2 = Integer.parseInt(x1.charAt(1) + "");
+            myarray2[x2][y2] = "*";
+//проверяем ход
+            if (myarray[x2][y2] == 1) {
+                count ++;
+                myarray2[x2][y2] = "X";
+                if (count == 3) System.out.println("ПОБЕДА!!!");
+                else System.out.println("Корабль подбит!!! ");
+            }
+            else System.out.println("мимо");
         }
-        else System.out.println("мимо");
+//игра окончена! Распечатаем результат
+        System.out.print("  ");
+        for (int k = 0; k < myarray2.length; k++) {
+            System.out.print(k + " ");
         }
+        System.out.println();
+
+        for (int g = 0; g < myarray2.length; g++){
+            System.out.print((g) + " ");
+            for (int h = 0; h < myarray2[0].length; h++){
+                if (myarray2[h][g].equals("*")){
+                }else if(myarray2[h][g].equals("X")) {
+                }else {
+                    myarray2[h][g] = ".";
+                }
+                System.out.print(myarray2[h][g]+" ");
+            }
+            System.out.println();
+        }
+
+
+
     }
 }
