4794cbc270a689d3c5d3a0fff98d1c4c1b66c848
Slinick <119591833+Slinick@users.noreply.github.com>
2023-01-12 10:27:38 +0100

    780

--- src/Practice/TaskFromBook1/MorskojBoj/Boj.java
+++ src/Practice/TaskFromBook1/MorskojBoj/Boj.java
@@ -1,62 +1,53 @@
 package Practice.TaskFromBook1.MorskojBoj;
 
-import java.lang.ref.Reference;
 import java.util.Scanner;
 
 public class Boj {
 public void morskojBoj(int[][] array){
-
-    int count = 0;
-    int i = 0; int j = 0;
-    String[][] myarray2 = new String[4][4];
-    for (i = 0; i < myarray2.length; i++){
-        for (j = 0; j < myarray2[0].length; j++){
-            myarray2[i][j] = ".";
-            System.out.print(myarray2[i][j]+" ");
-        }
-        System.out.println();
-    }
-    do {
-        //сделаем визуальную картинку
-//        String[][] myarray2 = new String[3][3];
-        int g = 0;
-        int h = 0;
-        for (g = 0; g < myarray2.length; g++){
-           for (h = 0; h < myarray2[0].length; h++){
-            if (myarray2[g][h].equals("*")){
-            }else if(myarray2[g][h].equals("X")) {
-
-            }else {
-                myarray2[g][h] = ".";
+//сделаем визуальную картинку из массива String. Второй массив нужен чтоб не выдавать корабли
+//инициализируем массив и все элементы массива
+    String[][] myarray2 = new String[array.length][array.length];
+        for (int i = 0; i < myarray2.length; i++){
+            for (int j = 0; j < myarray2[0].length; j++){
+                myarray2[i][j] = ".";
             }
-//            myarray2[i][j] = ".";
-                System.out.print(myarray2[g][h]+" ");
         }
-        System.out.println();
-    }
+    int count = 0;
+    while (count<3){
+        System.out.print("  ");
+            for (int k = 0; k < myarray2.length; k++) {
+                 System.out.print(k + " ");
+            }
+            System.out.println();
+                for (int g = 0; g < myarray2.length; g++){
+                    System.out.print((g) + " ");
+                        for (int h = 0; h < myarray2[0].length; h++){
+                            if (myarray2[h][g].equals("*")){
+                            }else if(myarray2[h][g].equals("X")) {
+                            }else {
+                                myarray2[h][g] = ".";
+                            }
+                            System.out.print(myarray2[h][g]+" ");
+                        }
+                    System.out.println();
+                }
 
     int[][] myarray = array;
     Scanner scaner2 = new Scanner(System.in);
-    System.out.println("введите x");
-    int x = scaner2.nextInt();
-    System.out.println("введите y");
-    int y = scaner2.nextInt();
-    myarray2[x][y] = "*";
+    System.out.println("Делайте выстрел. На поле есть корабль. (например 00)");
+    String x1 = scaner2.nextLine();
+    int x2 = Integer.parseInt(x1.charAt(0) + "");
+    int y2 = Integer.parseInt(x1.charAt(1) + "");
+    System.out.println(x2 + "   " + y2);
+    myarray2[x2][y2] = "*";
     //проверяем ход
-            if (myarray[x][y] == 1) {
+        if (myarray[x2][y2] == 1) {
             count ++;
-            myarray2[x][y] = "X";
-            if (count == 3) System.out.println("убит");
+            myarray2[x2][y2] = "X";
+            if (count == 3) System.out.println("ПОБЕДА!!!");
             else System.out.println("ранен");
-            }
-            else System.out.println("мимо");
-            }while (count<3);
-//    for (int i = 0; i < myarray.length; i++) {
-//        for (int j = 0; j < myarray[0].length; j++) {
-//            System.out.print(myarray[i][j]+" ");
-//        }
-//        System.out.println();
-//    }
-
-}
+        }
+        else System.out.println("мимо");
+        }
+    }
 }
--- src/Practice/TaskFromBook1/MorskojBoj/BojDemo.java
+++ src/Practice/TaskFromBook1/MorskojBoj/BojDemo.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 
 public class BojDemo {
     public static void main(String[] args) {
-        //int x = RequestParameters.gameSize("Введите размер поля");
 
+        int x = RequestParameters.gameSize("Введите размер поля");
         //заполним игровое поле кораблями
         SetShip set1 = new SetShip();
-        int [][] array1 =  set1.create(4);
+        int [][] array1 =  set1.create(x);
         //начинаем игру
         Boj boy1 = new Boj();
         boy1.morskojBoj(array1);
