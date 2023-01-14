cf4485d6c12915f6fd1159a04039379c0b3d7b04
Slinick <119591833+Slinick@users.noreply.github.com>
2023-01-14 18:58:08 +0100

    99

--- src/Practice/TaskFromBook1/MorskojBoj/GameBoard.java
+++ src/Practice/TaskFromBook1/MorskojBoj/GameBoard.java
@@ -4,39 +4,74 @@ import java.util.Arrays;
 import java.util.Random;
 
 class GameBoard {
-    int[][]gameBoard;
+    int[][] gameBoard;
     int size;
 
     void createField() {
         gameBoard = new int[size][size];
     }
 
-    void setShip (int shipSize){
+    void setShip(int shipSize) {
         int x = (int) (Math.random() * size);
         int y = (int) (Math.random() * (size - 2));
         //в том случае если корабль имеет вертикальное расположение
         Random isShipVertical = new Random();
-        if(isShipVertical.nextBoolean()){//размещаем наш случайный ВЕРТИКАЛЬНЫЙ корабль
-            gameBoard[x][y] = 1;
-            gameBoard[x][y + 1] = 1;
-            gameBoard[x][y + 2] = 1;
-        }else{//горизонтальный корабль
+        if (isShipVertical.nextBoolean()) {//размещаем наш случайный ВЕРТИКАЛЬНЫЙ корабль
             gameBoard[y][x] = 1;
-            gameBoard[y+1][x] = 1;
-            gameBoard[y+2][x] = 1;
+            gameBoard[y + 1][x] = 1;
+            gameBoard[y + 2][x] = 1;
+        } else {//горизонтальный корабль. Меняем x y чтоб не выпасть из массива
+            x = x+y;  y = x-y; x = x-y;
+            gameBoard[y][x] = 1;
+            gameBoard[y][x + 1] = 1;
+            gameBoard[y][x + 2] = 1;
         }
+    }
+//метод пытается поставить на поле рандомным образом корабль 100 раз
+    boolean setAnotherShip(int setAnotherShipSize) {
+        int x = 0;
+        int y = 0;
+        boolean flag = false;
+        for (int z = 0; z < 100; z++) {
+            flag = false;
+            x = (int) (Math.random() * size);
+            y = (int) (Math.random() * (size - 2));
+//пишем хитрый цикл фор который будет откидывать значения выходящие за массив
+            for (int x1 = x - 1; x1 < x + 2; x1++) {
+                if (x1 < 0) x1 = 0;
+                if (x1 == size) break;
+                for (int y1 = y - 1; y1 < y + 4; y1++) {
+                    if (y1 < 0) y1 = 0;
+                    if (y1 > size - 1) break;
+                    if (gameBoard[y1][x1] == 1) {
+                        flag = true;
+                    }
+                }
+            }
+            if (!flag) break;
+        }
+        if (flag) {
+            System.out.println("не удалось поставить корабль");
+        } else {
+            gameBoard[y][x] = 1;
+            gameBoard[y + 1][x] = 1;
+            gameBoard[y + 2][x] = 1;
+        }
+        return !flag;
     }
 
     int[][] boardReturner(){
         return gameBoard;
     }
 
-    @Override
-    public String toString() {
-        return "GameBoard{" +
-                "gameBoard=" + Arrays.deepToString(gameBoard) +
-                ", size=" + size +
-                '}';
+    void print(){
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(gameBoard[i][j]+" ");
+            }
+            System.out.println();
+        }
+
     }
 
     public int getSize() {
--- src/Practice/TaskFromBook1/MorskojBoj/RequestParameters.java
+++ src/Practice/TaskFromBook1/MorskojBoj/RequestParameters.java
@@ -5,18 +5,20 @@ import java.util.Scanner;
 class RequestParameters {
     static int parameterRequestInt(String message){
         boolean flag = true;
-        int size = 0;
+        String size = "";
+        String test = "";
         while (flag) {
             System.out.println(message);
             Scanner scanner = new Scanner(System.in);
-            size = scanner.nextInt();
+            size = scanner.nextLine();
             for (int i = 3; i <10 ; i++) {
-                if (size == i){
+                test = i + "";
+                if (size.equals(test)){
                     flag = false;
                 }
             }
         }
-        return size;
+        return Integer.parseInt(size);
     }
 
     static String parameterRequestStr(String message, int size){
@@ -27,27 +29,54 @@ class RequestParameters {
         String mychar2;
 
         while (flag || flag1) {
+            flag = true;
+            flag1 = true;
             char mychar = 'a';
             System.out.println(message);
             Scanner scanner = new Scanner(System.in);
             str = scanner.nextLine();
             str = str.toLowerCase();
-            mychar1 = str.charAt(0) + "";
-            mychar2 = str.charAt(1) + "";
 
-            for (int i = 0 ; i <size; i++) {
-                String myI = (i+1) + "";
-                if(mychar1.equals(mychar+"") && str.length()==2){
-                    flag = false;
-                }
-                if(mychar2.equals(myI) && str.length()==2){
-                    flag1 = false;
-                }
+            if (str.length() == 2) {
+                mychar2 = str.charAt(0) + "";
+                mychar1 = str.charAt(1) + "";
+
+                for (int i = 0; i < size; i++) {
+                    String myI = (i + 1) + "";
+                    if (mychar1.equals(mychar + "")){
+                        flag = false;
+                    }
+                    if (mychar2.equals(myI)) {
+                        flag1 = false;
+                    }
 //                    System.out.println("проход" + i + " чар" + mychar + " " + mychar1 +
 //                            " " + mychar + " " + flag + flag1);
-                mychar++;
+                    mychar++;
+                }
             }
         }
         return str;
     }
+
+    static boolean isAddShip (String message){
+        boolean yesOrNo = false;
+        boolean flag = true;
+        String str="";
+        while(flag) {
+            System.out.println(message);
+            Scanner scanner = new Scanner(System.in);
+            str = scanner.nextLine();
+            str = str.toLowerCase();
+            if (str.equals("y")) {
+                yesOrNo = true;
+                flag = false;
+            } else if (str.equals("n")) {
+                yesOrNo = false;
+                flag = false;
+            } else
+                flag = true;
+        }
+        return yesOrNo;
+    }
+
 }
