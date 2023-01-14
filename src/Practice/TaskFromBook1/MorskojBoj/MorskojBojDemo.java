057c18c1cc42112f7556636b7ecce3f80b1d7a45
Slinick <119591833+Slinick@users.noreply.github.com>
2023-01-14 21:55:25 +0100

    Merge remote-tracking branch 'github/master'

--- src/Practice/TaskFromBook1/MorskojBoj/MorskojBojDemo.java
+++ src/Practice/TaskFromBook1/MorskojBoj/MorskojBojDemo.java
@@ -8,16 +8,16 @@ class MorskojBojDemo {
         MorskojBoj_1_02 boj = new MorskojBoj_1_02();
 
         int size = RequestParameters.parameterRequestInt("Введите размер поля(от 3 до 9)");
-        boj.shotToWin = 3; //
-        boj.size = size;
-        boj.gameBoardStrCreator();
-        boj.firstBoardPrint(size);
+        boj.shotToWin = 3; //устанавлеваем счетчик на 3 попадания
+        boj.size = size;   //настраиваю размер игры
+        boj.gameBoardStrCreator();  //создаю массив
+        boj.firstBoardPrint(size);  //печатаю первое поле
 
         polozenieKorablia.size = size;
         polozenieKorablia.createField();
-        polozenieKorablia.setShip(2);
+        polozenieKorablia.setShip(2);        //ставлю первый случайный корабль
 
-        while(true) {
+        while(true) {                        //ставлю еще корабли по желанию
             boolean newShip = RequestParameters.isAddShip("На поле уже есть корабль! Поставить еще один? Y/N");
             if (!newShip) break;
             if(polozenieKorablia.setAnotherShip(2)) {
@@ -26,12 +26,12 @@ class MorskojBojDemo {
             }else System.out.println("не получилось разместить корабль");
         }
 
-        polozenieKorablia.print();
+        //polozenieKorablia.print();      //выводит готовое поле с кораблями для отладки
 
         //начинаем игру
         boj.gameBoardInt = polozenieKorablia.boardReturner();
         boj.gameBoardStrCreator();
 
-        boj.game();
+        boj.game();                       //запуск игры
     }
 }
--- src/Practice/TaskFromBook1/MorskojBoj/RequestParameters.java
+++ src/Practice/TaskFromBook1/MorskojBoj/RequestParameters.java
@@ -3,7 +3,7 @@ package Practice.TaskFromBook1.MorskojBoj;
 import java.util.Scanner;
 
 class RequestParameters {
-    static int parameterRequestInt(String message){
+    static int parameterRequestInt(String message){//метод просит размер игры и тестирует ввод
         boolean flag = true;
         String size = "";
         String test = "";
@@ -13,52 +13,52 @@ class RequestParameters {
             size = scanner.nextLine();
             for (int i = 3; i <10 ; i++) {
                 test = i + "";
-                if (size.equals(test)){
+                if (size.equals(test)){           //только цифра 3-9 пройдет проверку
                     flag = false;
                 }
             }
         }
-        return Integer.parseInt(size);
+        return Integer.parseInt(size);          //возвращаю размер игры в интежер
     }
 
     static String parameterRequestStr(String message, int size){
-        String str = "";
-        boolean flag = true;
-        boolean flag1 = true;
+        String str = "";                 //метод запрашивает ход и примет только букву +цифру
+        boolean flag = true;            //флаг для цифры
+        boolean flag1 = true;           //флаг для буквы
         String mychar1;
         String mychar2;
 
-        while (flag || flag1) {
+        while (flag || flag1) {       //спрашивать пока не будет корректного ввода
             flag = true;
             flag1 = true;
-            char mychar = 'a';
+            char mychar = 'a';           //поле универсального размера а значит буду через чар задавать координату
             System.out.println(message);
             Scanner scanner = new Scanner(System.in);
             str = scanner.nextLine();
-            str = str.toLowerCase();
+            str = str.toLowerCase();         //на всякий случай к нижнему регистру
 
             if (str.length() == 2) {
-                mychar2 = str.charAt(0) + "";
-                mychar1 = str.charAt(1) + "";
+                mychar2 = str.charAt(0) + "";   //разбиваю ввод на 2 символа
+                mychar1 = str.charAt(1) + "";   //второй символ
 
                 for (int i = 0; i < size; i++) {
-                    String myI = (i + 1) + "";
-                    if (mychar1.equals(mychar + "")){
-                        flag = false;
+                    String myI = (i + 1) + "";   //привожу символ к строке
+                    if (mychar1.equals(mychar + "")){   //только буква a b c d... пройдет проверку
+                        flag = false;                //упускаем флаг при успехе
                     }
-                    if (mychar2.equals(myI)) {
-                        flag1 = false;
+                    if (mychar2.equals(myI)) {    //только цифра пройдет проверку
+                        flag1 = false;            //опускаем флаг при успехе
                     }
 //                    System.out.println("проход" + i + " чар" + mychar + " " + mychar1 +
 //                            " " + mychar + " " + flag + flag1);
-                    mychar++;
+                    mychar++;  //char приростает. Это для универсальности поля
                 }
             }
         }
-        return str;
+        return str;     //возвращаем проверенную строку в формате 1a
     }
 
-    static boolean isAddShip (String message){
+    static boolean isAddShip (String message){ //метод спрашивает Y/N и примет только корректный ввод
         boolean yesOrNo = false;
         boolean flag = true;
         String str="";
@@ -66,7 +66,7 @@ class RequestParameters {
             System.out.println(message);
             Scanner scanner = new Scanner(System.in);
             str = scanner.nextLine();
-            str = str.toLowerCase();
+            str = str.toLowerCase();              //на всякий случай к нижнему регистру
             if (str.equals("y")) {
                 yesOrNo = true;
                 flag = false;
